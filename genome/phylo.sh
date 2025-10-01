76ff9ed17c8c8369866f04469cf6fa55ec70fd10
MicrobeLab <52490989+MicrobeLab@users.noreply.github.com>
2025-10-01 12:03:35 +0800

    Add files via upload

--- genome/phylo.sh
+++ genome/phylo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 phylophlan -i /path/to/genomes \
--d /path/to/phylophlan \
---diversity low \
--f supermatrix_aa.cfg \
--o /path/to/output 
---nproc 16  
+    -d /path/to/phylophlan \
+    --diversity low \
+    -f supermatrix_aa.cfg \
+    -o /path/to/output 
+    --nproc 16  
